ec96b0eaf74441ab3000c64423a7df91e302c99e
Jun Yap <rolandoremolaciojr@gmail.com>
2025-10-20 19:29:40 +0800

    Simplify config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
