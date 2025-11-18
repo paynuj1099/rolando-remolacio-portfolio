fac1336510c62c6cbcfa832c38bd6f5c532ba505
Rolando Remolacio <rolandojrremolacio@gmail.com>
2025-11-18 14:42:29 +0800

    Added Next.js Config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/ads.txt',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8',
+          },
+        ],
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
