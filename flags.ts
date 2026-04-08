833710ee5eedf180d3114dd74f13d3b4cbdc0faa
rolando-remolacio <rolando.remolacio@lawpractice.ai>
2026-04-09 06:19:43 +0800

    Fix build errors

--- flags.ts
+++ flags.ts
@@ -1,7 +1,7 @@
 import { flag } from 'flags/next';
 import { vercelAdapter } from '@flags-sdk/vercel';
 
-export const showResume = flag({
+export const showResume = flag<boolean>({
   key: 'show-resume',
   description: 'Show resume section',
   adapter: vercelAdapter(),
