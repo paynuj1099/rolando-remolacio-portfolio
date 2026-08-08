4d105022b70c32237c051d369f939d69da78a1bf
paynuj1099 <rolandojrremolacio@gmail.com>
2026-08-08 17:58:55 +0800

    Update projects

--- flags.ts
+++ flags.ts
@@ -4,7 +4,7 @@ import { vercelAdapter } from '@flags-sdk/vercel';
 export const showResume = flag<boolean>({
   key: 'show-resume',
   description: 'Show resume section',
-  adapter: process.env.FLAGS ? vercelAdapter() : undefined,
+  adapter: process.env.FLAGS && process.env.FLAGS_SDK_KEY ? vercelAdapter() : undefined,
   decide: () => process.env.SHOW_RESUME === 'true',
   defaultValue: false,
 });
